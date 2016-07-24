febd0e79f1ae35a8783531d5edcb8996bc305266
David Lynch <kemayo@gmail.com>
2016-07-24 00:03:59 -0500

    Profession icons: new tradeskill API

--- skills.lua
+++ skills.lua
@@ -12,6 +12,7 @@ local skills = {
         [1780] = false, -- Second That Emotion
     },
 }
+hskills = skills
 
 function mod:OnLoad()
     if IsAddOnLoaded("Blizzard_TradeSkillUI") then
@@ -30,7 +31,8 @@ function mod:ADDON_LOADED(event, addon)
 end
 
 function mod:Hooks()
-    hooksecurefunc("TradeSkillFrame_Update", function() mod:TradeSkillFrame_Update() end)
+    hooksecurefunc(TradeSkillFrame.RecipeList, "RefreshDisplay", function(self) mod:RefreshDisplay(self) end)
+    hooksecurefunc(TradeSkillFrame.RecipeList, "update", function(self) mod:RefreshDisplay(self) end)
 end
 
 local icon_cache = {}
@@ -39,19 +41,23 @@ local function button_onenter(self)
     if not (icon and icon.name and icon:IsVisible()) then
         return
     end
-    local skill = GetTradeSkillLine()
+    local _, skill = C_TradeSkillUI.GetTradeSkillLine()
     if not (skill and skills[skill]) then
         return
     end
     GameTooltip:SetOwner(self, "ANCHOR_NONE")
     GameTooltip:SetPoint("TOPLEFT", self, "TOPRIGHT")
     GameTooltip:AddLine(SUMMARY_ACHIEVEMENT_INCOMPLETE)
+
     for achievementid, recipes in pairs(skills[skill]) do
-        if recipes[icon.name] then
-            local _, name = GetAchievementInfo(achievementid)
-            GameTooltip:AddLine(name)
+        if recipes and recipes[icon.name] then
+            local _, name, _, complete = GetAchievementInfo(achievementid)
+            if not complete then
+                GameTooltip:AddLine(name)
+            end
         end
     end
+
     GameTooltip:Show()
 end
 local function button_onleave(self)
@@ -63,7 +69,7 @@ function mod:GetIconForTradeSkillLine(button, name)
     end
     local icon = icon_cache[button]
     if not icon then
-        icon = button:CreateTexture()
+        icon = button:CreateTexture(nil, "OVERLAY")
         icon:SetTexture([[Interface\ACHIEVEMENTFRAME\UI-Achievement-TinyShield]])
         icon:SetWidth(20)
         icon:SetHeight(20)
@@ -77,44 +83,39 @@ function mod:GetIconForTradeSkillLine(button, name)
     icon:Show()
 end
 
-function mod:TradeSkillFrame_Update()
+function mod:RefreshDisplay(frame)
     for button, icon in pairs(icon_cache) do
-        icon.name = nil
+        icon.achievementid = nil
         icon:Hide()
     end
 
-    local skill = GetTradeSkillLine()
+    local _, skill = C_TradeSkillUI.GetTradeSkillLine()
     if not (skill and skills[skill]) then
         return
     end
 
-    -- Yes, this bears some resemblance to what TradeSkillFrame_Update does
-    local offset = FauxScrollFrame_GetOffset(TradeSkillListScrollFrame)
-    local filter = TradeSkillFilterBar:IsShown()
-    local num_skills = filter and (TRADE_SKILLS_DISPLAYED - 1) or TRADE_SKILLS_DISPLAYED
-    for i=1, num_skills do
-        local name, type = GetTradeSkillInfo(i + offset)
-        if name and type ~= "header" then
-            local achievementid, criteriaid, achievement_done, criteria_done = self:CheckRecipe(skill, name)
-            if achievementid then
-                if core.db.done_achievements or not achievement_done then
-                    if --[[ core.db.done_criteria or --]] not criteria_done then
-                        local button = _G["TradeSkillSkill" .. (filter and (i + 1) or i)]
-                        self:GetIconForTradeSkillLine(button, name)
-                    end
+    -- This is mostly a copy of TradeSkillRecipeListMixin.RefreshDisplay
+    local offset = HybridScrollFrame_GetOffset(frame)
+    for i, tradeSkillButton in ipairs(frame.buttons) do
+        local tradeSkillInfo = frame.dataList[offset + i]
+        if tradeSkillInfo and tradeSkillInfo.type == "recipe" then
+            local achievementid, criteriaid, achievement_done, criteria_done = self:CheckRecipe(skill, tradeSkillInfo.name)
+            if achievementid and core.db.done_achievements or not achievement_done then
+                if --[[ core.db.done_criteria or --]] not criteria_done then
+                    self:GetIconForTradeSkillLine(tradeSkillButton, tradeSkillInfo.name)
                 end
             end
         end
     end
 end
 
-function mod:CheckRecipe(skill, name)
-    local achievements = skills[skill]
-    if not achievements then
+local achievements_loaded = false
+function mod:LoadAchievements()
+    if achievements_loaded then
         return
     end
-    for achievementid, recipes in pairs(achievements) do
-        if recipes == false then
+    for skill, achievements in pairs(skills) do
+        for achievementid, recipes in pairs(achievements) do
             recipes = {}
             for i=1, GetAchievementNumCriteria(achievementid) do
                 local desc, _, _, _, _, _, _, _, _, criteriaid = GetAchievementCriteriaInfo(achievementid, i)
@@ -124,7 +125,18 @@ function mod:CheckRecipe(skill, name)
                 end
             end
         end
-        if recipes[name] then
+    end
+    achievements_loaded = true
+end
+
+function mod:CheckRecipe(skill, name)
+    local achievements = skills[skill]
+    if not achievements then
+        return
+    end
+    self:LoadAchievements()
+    for achievementid, recipes in pairs(achievements) do
+        if recipes and recipes[name] then
             local _, a_name, _, complete = GetAchievementInfo(achievementid)
             local desc, _, done = GetAchievementCriteriaInfoByID(achievementid, recipes[name])
             return achievementid, recipes[name], complete, done
